8e6f30f12f8d9cd45bf45c1f81cbf618808c9342
carlospadron <padron.ca@gmail.com>
2024-11-09 14:26:09 +0000

    insert entries to dynamo db

--- csv_to_dynamo.py
+++ csv_to_dynamo.py
@@ -1,24 +1,32 @@
 import boto3
 import csv
+import os
 
 # Initialize a session using Amazon DynamoDB
 session = boto3.Session(
-    aws_access_key_id='YOUR_ACCESS_KEY',
-    aws_secret_access_key='YOUR_SECRET_KEY',
-    region_name='YOUR_REGION'
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    region_name=os.getenv('REGION_NAME', 'eu-west-2')
 )
 
 # Initialize DynamoDB resource
 dynamodb = session.resource('dynamodb')
 
 # Specify the table
-table = dynamodb.Table('YourTableName')
+table = dynamodb.Table('os_open_uprn')
 
 # Read CSV file
-with open('yourfile.csv', mode='r') as file:
+with open('osopenuprn_202404.csv', mode='r', encoding='utf-8-sig') as file:
     csv_reader = csv.DictReader(file)
+    n = 0
     for row in csv_reader:
+        print(n)
         # Write each row to DynamoDB
+        row = {k.lower(): v for k, v in row.items()}
+        row['uprn'] = int(row['uprn'])
+        #print(row)
         table.put_item(Item=row)
+        n += 1
+
 
 print("CSV data has been written to DynamoDB table.")
